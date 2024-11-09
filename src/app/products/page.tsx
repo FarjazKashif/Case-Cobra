ef83f1ccb13877ba511280aa9810b2670aab2fb5
Farjaz Kashif <farjazkashif01@gmail.com>
2024-11-09 13:52:25 +0500

    Category Tabs added

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,34 +1,36 @@
-'use client'
+'use client';
 
 import React, { useState, useEffect } from 'react';
 import { client } from '@/sanity/lib/client';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import Image from 'next/image';
 import { urlFor } from '@/sanity/lib/image';
-import { ArrowBigLeft, ArrowBigRight, ArrowLeft, ArrowLeftIcon, ArrowRight, ShoppingCart } from 'lucide-react';
+import { ArrowLeft, ArrowRight, ShoppingCart } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Skeleton } from '@/components/ui/skeleton';
 import Link from 'next/link';
 import { Sheet, SheetContent, SheetClose, SheetTrigger } from '@/components/ui/sheet';
-import { toast } from "sonner"
-import { ScrollArea } from "@/components/ui/scroll-area"
+import { toast } from "sonner";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/ShadcnTabs/tabs';
 
 export interface IProduct {
-  title: string
-  price: number
-  image: string
-  currentSlug: string
+  title: string;
+  price: number;
+  image: string;
+  currentSlug: string;
   category: {
-    category: string
-  }
+    category: string;
+  };
 }
 
 // Products per page
 const PRODUCTS_PER_PAGE = 12;
 
-const getProductsData = async () => {
+const getProductsData = async (category: string | null = null) => {
+  const filter = category ? `&& category->category == "${category}"` : '';
   const products = await client.fetch(
-    `*[_type=="product"] | order(_createdAt desc){
+    `*[_type=="product" ${filter}] | order(_createdAt desc){
       title,
       price,
       image,
@@ -45,22 +47,21 @@ const ProductsPage = () => {
   const [data, setData] = useState<IProduct[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useState(true);
-  const [cartState, setCartState] = useState(false)
   const [cartItems, setCartItems] = useState<IProduct[]>([]);
   const [isCartOpen, setIsCartOpen] = useState(false);
-
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true)
-      const products = await getProductsData();
+      setLoading(true);
+      const products = await getProductsData(selectedCategory);
       setData(products);
-      setLoading(false)
+      setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [selectedCategory]);
 
-  // Calculate the current products to display
+  // Pagination logic
   const indexOfLastProduct = currentPage * PRODUCTS_PER_PAGE;
   const indexOfFirstProduct = indexOfLastProduct - PRODUCTS_PER_PAGE;
   const currentProducts = data.slice(indexOfFirstProduct, indexOfLastProduct);
@@ -83,73 +84,74 @@ const ProductsPage = () => {
         <MaxWidthWrapper className='pt-10 pb-32 sm:pb-12 lg:pt-24 xl:pt-32'>
           <div className='relative bg-green-500 w-full h-36 rounded-3xl pb-32 grid xl:grid-cols-2'>
             <div className='px-10 py-2'>
-              <h1 className='text-2xl font-bold text-white'>Get Premium Quality t-shirts...</h1>
+              <h2 className='text-2xl font-bold text-white'>Get Premium Quality T-Shirts...</h2>
               <p className='text-white text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident enim recusandae excepturi praesentium.</p>
             </div>
-
             <div className='relative box-border'>
               <Image className='absolute right-0 -top-[123px]' src="/model.png" width={400} height={500} alt='Model' />
             </div>
-
           </div>
-          <Sheet open={isCartOpen} onOpenChange={setIsCartOpen}>
-            <SheetTrigger asChild>
-              <Button variant="outline" size="sm">
-                <ShoppingCart className='text-zinc-950' />
-              </Button>
-            </SheetTrigger>
-            <SheetContent className='bg-white flex flex-col'>
-              <h2 className='text-lg font-semibold'>Your Cart</h2>
-              <ScrollArea className='flex-1 mt-4 overflow-y-auto'>
-                {cartItems.length === 0 ? (
-                  <p>Your cart is empty.</p>
-                ) : (
-                  cartItems.map((product, index) => (
-                    <div key={index} className='flex items-center justify-between py-2'>
-                      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                          <tr>
-                            <th scope="col" className="px-6 py-3">
-                              Image
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                              Price
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                              
-                            </th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                              <Image src={urlFor(product.image).url()} alt={product.title} width={50} height={50} />
-                            </th>
-                            <td className="px-6 py-4">
-                              <span>Rs.{product.price}</span>
-                            </td>
-                            <td className="px-6 py-4">
-                              <Button variant="destructive" onClick={() => {
-                                setCartItems((prevI) => prevI.filter((_, i) => i != index))
-                              }}>
-                                Undo
-                              </Button>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  ))
-                )}
-              </ScrollArea>
-              <SheetClose asChild>
-                <Button variant="outline">Close</Button>
-              </SheetClose>
-            </SheetContent>
-          </Sheet>
+
+          <div className='flex justify-between mt-10 mb-10'>
+            <Tabs defaultValue="All" className="w-[600px]">
+              <TabsList className="grid w-fit gap-x-1 grid-cols-5 bg-gray-200/35 rounded-lg p-1">
+                {['All', 'T-Shirt', 'Polos', 'Hoodies', 'Sweat Shirts'].map((cat) => (
+                  <TabsTrigger
+                    key={cat}
+                    value={cat.toLowerCase()}
+                    onClick={() => setSelectedCategory(cat == 'All' ? null : cat)}
+                    className={`py-1 px-4 text-center font-semibold rounded-lg text-gray-700 hover:bg-gray-200 transition-colors duration-200 
+                      ${selectedCategory === (cat === 'All' ? null : cat) ? 'bg-gray-200 text-green-500' : ''} 
+                      data-[state=active]:bg-gray-200 data-[state=active]:text-green-500`}
+                  >
+                    {cat}
+                  </TabsTrigger>
+                ))}
+              </TabsList>
+            </Tabs>
+
+            <Sheet open={isCartOpen} onOpenChange={setIsCartOpen}>
+              <SheetTrigger asChild>
+                <Button variant="outline" size="sm">
+                  <ShoppingCart className='text-green-600' />
+                </Button>
+              </SheetTrigger>
+              <SheetContent className='bg-white flex flex-col'>
+                <h2 className='text-lg font-semibold'>Your Cart</h2>
+                <ScrollArea className='flex-1 mt-4 overflow-y-auto'>
+                  {cartItems.length === 0 ? (
+                    <p>Your cart is empty.</p>
+                  ) : (
+                    cartItems.map((product, index) => (
+                      <div key={index} className='flex items-center justify-between py-2'>
+                        <table className="w-full text-sm text-left text-gray-500">
+                          <tbody>
+                            <tr className="bg-white border-b">
+                              <td className="px-6 py-4">
+                                <Image src={urlFor(product.image).url()} alt={product.title} width={50} height={50} />
+                              </td>
+                              <td className="px-6 py-4">Rs.{product.price}</td>
+                              <td className="px-6 py-4">
+                                <Button variant="destructive" onClick={() => setCartItems((prevItems) => prevItems.filter((_, i) => i !== index))}>
+                                  Undo
+                                </Button>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    ))
+                  )}
+                </ScrollArea>
+                <SheetClose asChild>
+                  <Button variant="outline">Close</Button>
+                </SheetClose>
+              </SheetContent>
+            </Sheet>
+          </div>
+
           <div className='grid grid-cols-2 gap-x-3 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 sm:gap-x-2 xl:gap-x-8'>
             {loading ? (
-              // Show skeletons if loading
               Array.from({ length: PRODUCTS_PER_PAGE }).map((_, index) => (
                 <div key={index} className='rounded-xl my-3 relative shadow-md hover:shadow-lg ease-in duration-100 backdrop-blur-lg bg-white/15 col-span-1'>
                   <Skeleton className='w-full h-72' />
@@ -161,8 +163,8 @@ const ProductsPage = () => {
               ))
             ) : (
               currentProducts.map((item, idx) => (
-                <div className='rounded-xl my-3 relative shadow-md hover:shadow-lg ease-in duration-100 backdrop-blur-lg bg-white/15 col-span-1'>
-                  <Link href={`products/${item.currentSlug}`} key={idx}>
+                <div key={idx} className='rounded-xl my-3 relative shadow-md hover:shadow-lg ease-in duration-100 backdrop-blur-lg bg-white/15 col-span-1'>
+                  <Link href={`products/${item.currentSlug}`}>
                     <div className='relative box-border overflow-hidden'>
                       <Image className='ease-in-out duration-1000 hover:scale-110 object-contain h-[250px]' src={urlFor(item.image).url()} alt={item.title} width={300} height={300} />
                     </div>
@@ -171,15 +173,11 @@ const ProductsPage = () => {
                     <div className='flex justify-between items-center pb-4'>
                       <p className='text-xs text-rose-700'>{item.category.category.toLowerCase()}</p>
                       <Button variant="outline" size="sm" onClick={() => {
-                        setCartItems((prevItems) => [...prevItems, item]); // Add the product to cart
-                        // setIsCartOpen(true); // Open the cart sheet 
-                        toast(`${item.title.toWellFormed()} has been added!`, {
+                        setCartItems((prevItems) => [...prevItems, item]);
+                        toast(`${item.title} has been added!`, {
                           description: "Check the Cart...",
-                          style: {
-                            backgroundColor: 'green',
-                            color: "white",
-                          }
-                        })
+                          style: { backgroundColor: 'green', color: "white" }
+                        });
                       }}>
                         <ShoppingCart className='h-4 w-4 text-green-700' />
                       </Button>
@@ -196,8 +194,6 @@ const ProductsPage = () => {
           </div>
         </MaxWidthWrapper>
 
-        {/* Pagination */}
-
         <div className="flex justify-center space-x-4 my-2">
           <Button className='text-zinc-950 text-xs' variant="ghost" size="sm" onClick={handlePrevPage} disabled={currentPage === 1}>
             <ArrowLeft className='w-5 h-5' />
@@ -207,7 +203,7 @@ const ProductsPage = () => {
           </Button>
         </div>
       </section>
-    </div >
+    </div>
   );
 };
 
